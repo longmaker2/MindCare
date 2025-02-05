529e222541fea32369b2278bc335d63e9cbee959
Ericanshimir <109414922+Ericanshimir@users.noreply.github.com>
2025-02-05 12:46:19 +0300

    Delete eyt directory

--- eyt/myapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Feature
-from .models import Chart
-from .models import Video
-
-# Register your models here.
-admin.site.register(Feature)
-admin.site.register(Chart)
-admin.site.register(Video)
